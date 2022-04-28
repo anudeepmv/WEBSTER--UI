23c65d9167493f239460ce7a703800e2f4b194a8
Cherukuri.31@wright.edu <Cherukuri.31@wright.edu>
2022-04-28 23:18:02 +0530

    as developer i have add test case for admindashboard and header

--- src/Test/admindashboard.test.js
+++ src/Test/admindashboard.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, } from '@testing-library/react';
 import AdminDashboard from '../pages/adminDashbord/index';
 
 test('renders learn react link', () => {
   render(<AdminDashboard/>);
   const linkElement = screen.getByText(/Welcome to Gala/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+
+it("should close drawer", () => {
+  const { getByTestId } = render(<AdminDashboard />);
+  fireEvent.click(getByTestId("menuButton"));
+  fireEvent.click(getByTestId("close"));
+  expect(getByTestId("drawer")).not.toBeVisible(); // This doesn't work...
 });
