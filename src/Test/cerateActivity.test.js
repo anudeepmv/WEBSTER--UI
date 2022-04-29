cdca679525778225ea74c586b26acc18a1718d45
vinaychakka <chakka.10@wright.edu>
2022-04-30 02:28:34 +0530

    this test case is about onchange in create activity

--- src/Test/cerateActivity.test.js
+++ src/Test/cerateActivity.test.js
@@ -6,3 +6,14 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/create activity/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+it('should call onChange prop', () => {
+  const onSearchMock = jest.fn();
+  const event = {
+    preventDefault() {},
+    target: { value: 'the-value' }
+  };
+  const component = enzyme.shallow(<CreateActivity onSearch={onSearchMock} />);
+  component.find('input').simulate('change', event);
+  expect(onSearchMock).toBeCalledWith('the-value');
+});
