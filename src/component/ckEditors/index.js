53713d0ce01e3e67318e3aae406bd396911f631a
janu0613 <bandi.30@wright.edu>
2022-04-29 11:00:53 +0530

    Reacting to resolve and reject the data

--- src/component/ckEditors/index.js
+++ src/component/ckEditors/index.js
@@ -1,3 +1,4 @@
+/// Reacting to resolve and reject the data
 import React from "react";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
