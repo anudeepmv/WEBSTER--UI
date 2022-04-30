1517e974ef66e5108a82c2c34693d4a1a521c8e3
janu0613 <bandi.30@wright.edu>
2022-04-30 07:48:10 +0530

    formatted documentation on render and screen, render to transmit the data from front end rear end

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,5 @@
+///formatted documentation on render and screen
+///render to transmit the data from front end rear end
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
